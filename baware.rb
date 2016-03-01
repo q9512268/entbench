19a67764e7f9ddb5abdb4bb0692b121742477cd8
acanino <anthony.canino1@gmail.com>
2016-03-01 16:12:07 -0500

    Adding energy saved table to baware.

--- baware.rb
+++ baware.rb
@@ -7,26 +7,26 @@ $BENCH = {sunflow:"sunflow", crypto:"crypto"}
 
 $DIR   = "baware_run"
 $RUNS = [
-  "run_sd_hc.txt",
-  "run_sd_hcu.txt",
-  "run_sd_mc.txt",
-  "run_sd_mcu.txt",
-  "run_sd_lc.txt",
-  "run_sd_lcu.txt",
+  "run_sd_hc.txt",  # 0
+  "run_sd_hcu.txt", # 1
+  "run_sd_mc.txt",  # 2
+  "run_sd_mcu.txt", # 3
+  "run_sd_lc.txt",  # 4
+  "run_sd_lcu.txt", # 5
 
-  "run_md_hc.txt",
-  "run_md_hcu.txt",
-  "run_md_mc.txt",
-  "run_md_mcu.txt",
-  "run_md_lc.txt",
-  "run_md_lcu.txt",
+  "run_md_hc.txt",  # 6
+  "run_md_hcu.txt", # 7
+  "run_md_mc.txt",  # 8
+  "run_md_mcu.txt", # 9
+  "run_md_lc.txt",  # 10
+  "run_md_lcu.txt", # 11
 
-  "run_ld_hc.txt",
-  "run_ld_hcu.txt",
-  "run_ld_mc.txt",
-  "run_ld_mcu.txt",
-  "run_ld_lc.txt",
-  "run_ld_lcu.txt"
+  "run_ld_hc.txt",  # 12
+  "run_ld_hcu.txt", # 13
+  "run_ld_mc.txt",  # 14
+  "run_ld_mcu.txt", # 15
+  "run_ld_lc.txt",  # 16
+  "run_ld_lcu.txt"  # 17
   ]
 
 $DATAS = ['"energy-saver"', '"managed"', '"full-throttle"']
@@ -68,3 +68,23 @@ $BENCH.each do |bench, path|
     consumeddat.write("\n")
   end
 end 
+
+# Dump energy saved tables
+puts "bench energy_saver/managed % managed/full_throttle % energy_saver/full_throttle %"
+rows = []
+$BENCH.each do |bench, path|
+  es_m = consumedtable[bench][10] - consumedtable[bench][11]
+  es_mp = (es_m / consumedtable[bench][10]) * 100.0
+
+  m_ft = consumedtable[bench][14] - consumedtable[bench][15]
+  m_ftp = (es_m / consumedtable[bench][14]) * 100.0
+
+  es_ft = consumedtable[bench][16] - consumedtable[bench][17]
+  es_ftp = (es_m / consumedtable[bench][16]) * 100.0
+  
+  rows << [bench, es_m, es_mp, m_ft, m_ftp, es_ft, es_ftp]
+  rows << :separator
+end
+
+table = Terminal::Table.new :headings => ["Bench", "energy_saver/managed", "%", "managed/full_throttle", "%", "energy_saver/full_throttle", "%"], :rows => rows
+puts table
